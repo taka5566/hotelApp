cd785b51acc7ae75fdc4f877cd92a9a7de6ca49c
taka5566 <139129841+taka5566@users.noreply.github.com>
2024-05-13 04:48:45 +0800

    backend

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ import PlacePage from './pages/PlacePage';
 import BookingPage from './pages/BookingPage';
 import BookingsPage from './pages/BookingsPage';
 
-axios.defaults.baseURL = 'http://localhost:4000';
+// axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'http://hotelApp.onrender.com';
+
 axios.defaults.withCredentials = true;
 
 function App() {
